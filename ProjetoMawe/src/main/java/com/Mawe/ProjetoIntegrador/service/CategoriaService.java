23af7db10a8c5105e9f17ff9497b8aec624bbc01
unknown <viniciuslavsilva@gmail.com>
2021-07-02 12:29:20 -0300

    Alterações no Projeto Mawe

--- ProjetoMawe/src/main/java/com/Mawe/ProjetoIntegrador/service/CategoriaService.java
+++ ProjetoMawe/src/main/java/com/Mawe/ProjetoIntegrador/service/CategoriaService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.Mawe.ProjetoIntegrador.DTO.CategoriaDTO;
 import com.Mawe.ProjetoIntegrador.model.Categoria;
 import com.Mawe.ProjetoIntegrador.repository.CategoriaRepository;
 
@@ -20,6 +21,7 @@ public class CategoriaService {
 	 * @since 1.0
 	 * @author Gustavo Moraes
 	 */
+	
 	public Optional<Object> adicionar(Categoria novacategoria){
 		return categoriarepository.findByCategoriaSustentavel(novacategoria.getCategoriaSustentavel()).
 				map(categoriaexistente->{
@@ -27,5 +29,23 @@ public class CategoriaService {
 		}).orElseGet(() -> {
 			return Optional.ofNullable(categoriarepository.save(novacategoria));
 		});
-	} 
+	}
+	/**Alterando uma Categoria
+	 * 
+	 * @param id
+	 * @param novacategoria
+	 * @return Retorna uma catergoria atualizada, ou um Op vazio.
+	 * @since 1.0
+	 * @author Vinícius
+	 */
+	public Optional<Categoria> alterar(Long id,CategoriaDTO novacategoria){
+		return categoriarepository.findById(id).map(atualizar-> {
+			atualizar.setCategoriaRanking(novacategoria.getCategoriaRanking());
+			atualizar.setCategoriaDoacao(novacategoria.getCategoriaDoacao());
+			return Optional.ofNullable(categoriarepository.save(atualizar));
+			
+		}).orElseGet(()->{
+			return Optional.empty();
+		});
+	}
 }
