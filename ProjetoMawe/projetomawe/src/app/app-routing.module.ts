1582803e5b03a14a09a2dfb83607a0c4d889f293
Gustavo Moraes <gustavomm.contato@gmail.com>
2021-07-28 14:58:05 -0300

    atualizando rotas

--- ProjetoMawe/projetomawe/src/app/app-routing.module.ts
+++ ProjetoMawe/projetomawe/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
+import { ContatoComponent } from './contato/contato.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: 'Home', pathMatch: 'full'},
+  {path:"home", component: HomeComponent },
+  {path:"contato", component:ContatoComponent },
+  {path:"sobre-nos", component:SobreNosComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
