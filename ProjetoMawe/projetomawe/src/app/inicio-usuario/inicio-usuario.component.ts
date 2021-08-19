caa934f2f944e4e26fef69a832618b5454b9fcd6
GuilhermeRodrigues <guilherme.rsantos@bol.com.br>
2021-08-19 11:00:58 -0300

    Adicionando Produtos nos usuarios

--- ProjetoMawe/projetomawe/src/app/inicio-usuario/inicio-usuario.component.ts
+++ ProjetoMawe/projetomawe/src/app/inicio-usuario/inicio-usuario.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { Categoria } from '../model/Categoria';
+import { produto } from '../model/produto';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
+import { CategoriaService } from '../service/categoria.service';
+import { ProdutoService } from '../service/produto.service';
 
 @Component({
   selector: 'app-inicio-usuario',
@@ -6,10 +14,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio-usuario.component.css']
 })
 export class InicioUsuarioComponent implements OnInit {
+  categoria: Categoria = new Categoria()
+  Produto: produto = new produto()
+  listaCategoria: Categoria[]
+  idCategoria: number
+  listaProdutos: produto[]
+  usuario: Usuario = new Usuario()
+  idusuario = environment.id
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private produtoService: ProdutoService,
+    private categoriaService: CategoriaService,
+    private authService: AuthService
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    if(environment.token == ''){
+      alert('Sua sessão expirou, por favor entre novamente!')
+      this.router.navigate(['/Logar'])
+    }
+      
+
+    this.getAllCategoria()
+    this.getAllProduto()
   }
+
+
+
+  getAllCategoria(){
+    this.listaCategoria = []
+
+   this.categoriaService.getAll().subscribe((resp: Categoria[]) => {
+     this.listaCategoria = resp
+   })
+
+   }
+ 
+getAllProduto(){
+ this.produtoService.getAllProduto().subscribe((resp: produto[]) => {
+     this.listaProdutos = resp
+
+ })
+}
+
+findByIdUsuario(){
+  this.authService.getByIdUsuario(this.idusuario).subscribe((resp: Usuario) => {
+    this.usuario = resp
+    console.log(this.usuario)
+
+  })
+}
 
 }
